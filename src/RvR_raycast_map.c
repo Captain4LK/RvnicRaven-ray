0bc72227f8d635447491ffdb1eeb909d5af36fd8
Captain4LK <captain4lk@tutanota.com>
2022-06-14 20:29:02 +0200

    RvR_ray: overhaul map loading/saving/creation code

--- src/RvR_raycast_map.c
+++ src/RvR_raycast_map.c
@@ -51,49 +51,48 @@ void RvR_ray_map_create(uint16_t width, uint16_t height)
    ray_map.ceil_tex = RvR_malloc(ray_map.width*ray_map.height*sizeof(*ray_map.ceil_tex));
    ray_map.floor = RvR_malloc(ray_map.width*ray_map.height*sizeof(*ray_map.floor));
    ray_map.ceiling = RvR_malloc(ray_map.width*ray_map.height*sizeof(*ray_map.ceiling));
-   memset(ray_map.wall_ftex,0,ray_map.width*ray_map.height*sizeof(*ray_map.wall_ftex));
-   memset(ray_map.wall_ctex,0,ray_map.width*ray_map.height*sizeof(*ray_map.wall_ctex));
+
+   memset(ray_map.floor,0,ray_map.width*ray_map.height*sizeof(*ray_map.floor));
    memset(ray_map.floor_tex,0,ray_map.width*ray_map.height*sizeof(*ray_map.floor_tex));
    memset(ray_map.ceil_tex,0,ray_map.width*ray_map.height*sizeof(*ray_map.ceil_tex));
-   memset(ray_map.floor,0,ray_map.width*ray_map.height*sizeof(*ray_map.floor));
-   memset(ray_map.ceiling,0,ray_map.width*ray_map.height*sizeof(*ray_map.ceiling));
+   memset(ray_map.wall_ftex,0,ray_map.width*ray_map.height*sizeof(*ray_map.wall_ftex));
+   memset(ray_map.wall_ctex,0,ray_map.width*ray_map.height*sizeof(*ray_map.wall_ctex));
 
    ray_map.sky_tex = 0;
    ray_map.sprite_count = 0;
 
    for(int y = 0;y<ray_map.height;y++)
-   {
       for(int x = 0;x<ray_map.width;x++)
-      {
-         ray_map.floor[y*ray_map.width+x] = 0;
          ray_map.ceiling[y*ray_map.width+x] = 32*128;
-      }
-   }
 }
 
 static void ray_map_free()
 {
-   if(ray_map.wall_ftex!=NULL)
-      RvR_free(ray_map.wall_ftex);
-   if(ray_map.wall_ctex!=NULL)
-      RvR_free(ray_map.wall_ctex);
-   if(ray_map.floor_tex!=NULL)
-      RvR_free(ray_map.floor_tex);
-   if(ray_map.ceil_tex!=NULL)
-      RvR_free(ray_map.ceil_tex);
    if(ray_map.floor!=NULL)
       RvR_free(ray_map.floor);
    if(ray_map.ceiling!=NULL)
       RvR_free(ray_map.ceiling);
+   if(ray_map.floor_tex!=NULL)
+      RvR_free(ray_map.floor_tex);
+   if(ray_map.ceil_tex!=NULL)
+      RvR_free(ray_map.ceil_tex);
+   if(ray_map.wall_ftex!=NULL)
+      RvR_free(ray_map.wall_ftex);
+   if(ray_map.wall_ctex!=NULL)
+      RvR_free(ray_map.wall_ctex);
+   if(ray_map.sprites!=NULL)
+      RvR_free(ray_map.sprites);
 
-   ray_map.wall_ftex = NULL;
-   ray_map.wall_ctex = NULL;
+   ray_map.floor = NULL;
    ray_map.floor_tex = NULL;
    ray_map.ceil_tex= NULL;
-   ray_map.floor = NULL;
    ray_map.ceiling = NULL;
+   ray_map.wall_ftex = NULL;
+   ray_map.wall_ctex = NULL;
+   ray_map.sprites = NULL;
    ray_map.width = 0;
    ray_map.height = 0;
+   ray_map.sprite_count = 0;
 }
 
 int RvR_ray_map_sprite_count()
@@ -103,13 +102,15 @@ int RvR_ray_map_sprite_count()
 
 RvR_ray_map_sprite *RvR_ray_map_sprite_get(unsigned index)
 {
-   if(index>=ray_map.sprite_count)
+   if(ray_map.sprites==NULL||index>=ray_map.sprite_count)
       return NULL;
    return &ray_map.sprites[index];
 }
 
 void RvR_ray_map_load_path(const char *path)
 {
+   RvR_error_check(path!=NULL,"RvR_ray_map_load_path","argument 'path' must be non-NULL\n");
+
    int32_t size = 0;
    RvR_rw rw_decomp;
    RvR_rw_init_path(&rw_decomp,path,"rb");
@@ -119,9 +120,12 @@ void RvR_ray_map_load_path(const char *path)
    RvR_rw rw;
    RvR_rw_init_const_mem(&rw,mem,size);
    RvR_ray_map_load_rw(&rw);
-
    RvR_rw_close(&rw);
+
    RvR_free(mem);
+
+RvR_err:
+   return;
 }
 
 void RvR_ray_map_load(uint16_t id)
@@ -137,33 +141,34 @@ void RvR_ray_map_load(uint16_t id)
    RvR_rw_init_const_mem(&rw_decomp,mem_pak,size_in);
    mem_decomp = RvR_decompress(&rw_decomp,&size_out);
    RvR_rw_close(&rw_decomp);
-
    RvR_free(mem_pak);
 
    RvR_rw rw;
    RvR_rw_init_const_mem(&rw,mem_decomp,size_out);
    RvR_ray_map_load_rw(&rw);
-
    RvR_rw_close(&rw);
+
    RvR_free(mem_decomp);
 }
 
 void RvR_ray_map_load_rw(RvR_rw *rw)
 {
+   RvR_error_check(rw!=NULL,"RvR_ray_map_load_rw","argument 'rw' must be non-NULL\n");
+
    //Read and check version
    uint16_t version = RvR_rw_read_u16(rw);
-   if(version!=0)
-   {
-      RvR_log_line("RvR_ray_map_load_rw","Invalid version '%d', expected version '0'\n",version);
-      return;
-   }
+   RvR_error_check(version==0,"RvR_ray_map_load_rw","Invalid version '%d', expected version '0'\n",version);
 
    //Read sky texture
-   ray_map.sky_tex = RvR_rw_read_u16(rw);
+   uint16_t sky_tex = RvR_rw_read_u16(rw);
 
    //Read level width and height
-   ray_map.width = RvR_rw_read_u16(rw);
-   ray_map.height = RvR_rw_read_u16(rw);
+   uint16_t width = RvR_rw_read_u16(rw);
+   uint16_t height = RvR_rw_read_u16(rw);
+   
+   //Create map
+   RvR_ray_map_create(width,height);
+   ray_map.sky_tex = sky_tex;
 
    //Read sprite count
    ray_map.sprite_count = RvR_rw_read_u32(rw);
@@ -171,13 +176,6 @@ void RvR_ray_map_load_rw(RvR_rw *rw)
 
    //Read texture, floor and ceiling
    int32_t tile_count = ray_map.width*ray_map.height;
-   ray_map.wall_ftex = RvR_malloc(tile_count*sizeof(*ray_map.wall_ftex));
-   ray_map.wall_ctex = RvR_malloc(tile_count*sizeof(*ray_map.wall_ctex));
-   ray_map.floor_tex = RvR_malloc(tile_count*sizeof(*ray_map.floor_tex));
-   ray_map.ceil_tex = RvR_malloc(tile_count*sizeof(*ray_map.ceil_tex));
-   ray_map.floor = RvR_malloc(tile_count*sizeof(*ray_map.floor));
-   ray_map.ceiling = RvR_malloc(tile_count*sizeof(*ray_map.ceiling));
-
    for(int32_t i = 0;i<tile_count;i++) ray_map.floor[i] = RvR_rw_read_u32(rw);
    for(int32_t i = 0;i<tile_count;i++) ray_map.ceiling[i] = RvR_rw_read_u32(rw);
    for(int32_t i = 0;i<tile_count;i++) ray_map.floor_tex[i] = RvR_rw_read_u16(rw);
@@ -198,10 +196,15 @@ void RvR_ray_map_load_rw(RvR_rw *rw)
       ray_map.sprites[i].extra1 = RvR_rw_read_u32(rw);
       ray_map.sprites[i].extra2 = RvR_rw_read_u32(rw);
    }
+
+RvR_err:
+   return;
 }
 
 void RvR_ray_map_save(const char *path)
 {
+   RvR_error_check(path!=NULL,"RvR_ray_map_save","argument 'path' must be non-NULL\n");
+
    //Calculate needed memory
    int size = 0;
    size+=2; //version
@@ -284,6 +287,9 @@ void RvR_ray_map_save(const char *path)
    //Free temp buffer
    RvR_rw_close(&rw);
    RvR_free(mem);
+
+RvR_err:
+   return;
 }
 
 int RvR_ray_map_inbounds(int16_t x, int16_t y)
